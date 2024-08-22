d80a7cd1ba130ce2de7ea0472ec076e290d8ef14
Cory McHattie <crmchattie@gmail.com>
2024-08-22 17:24:25 -0400

    Update projects page.

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -37,26 +37,27 @@ export default async function Project({ params }: Props) {
     <main className="max-w-6xl mx-auto lg:px-16 px-8">
       <div className="max-w-3xl mx-auto">
         <div className="flex items-start justify-between mb-4">
-          <h1 className="font-bold lg:text-5xl text-3xl lg:leading-tight mb-4">
-            {project.name}
-          </h1>
+          <div className="flex items-center justify-center space-x-4">
+            <Image
+              className="rounded-xl"
+              width={30}
+              height={30}
+              src={project.coverImage?.image || fallBackImage}
+              alt={project.coverImage?.alt || project.name}
+            />
+            <h1 className="font-bold lg:text-5xl text-3xl lg:leading-tight mb-0">
+              {project.name}
+            </h1>
+          </div>
 
           <a
             href={project.projectUrl}
             rel="noreferrer noopener"
-            className="bg-[#1d1d20] text-white hover:border-zinc-700 border border-transparent rounded-md px-4 py-2"
+            className="bg-[#1d1d20] text-white border-zinc-700 border border-transparent rounded-md px-4 py-2"
           >
-            Explore
+            Link to Project
           </a>
         </div>
-
-        <Image
-          className="rounded-xl border border-zinc-800"
-          width={900}
-          height={460}
-          src={project.coverImage?.image || fallBackImage}
-          alt={project.coverImage?.alt || project.name}
-        />
 
         <div className="flex flex-col gap-y-6 mt-8 leading-7 text-zinc-400">
           <PortableText value={project.description} />
